60ba74f6a8f65478f8fa7bdf0b1a9137f9c942c4
appleSimple <jy_4148@naver.com>
2023-08-02 23:44:14 +0900

    끌말잇기 class component 생성

--- 2.끝말잇기/WordRelay.jsx
+++ 2.끝말잇기/WordRelay.jsx
@@ -4,13 +4,61 @@ const ReactDOM = require('react-dom');
 
 class WordRelay extends Component {
   state = {
-    text: 'Hello, Webpack'
+    word: '지연',
+    value: '',
+    result: '',
   };
 
+  onSubmitForm = (e) => {
+    e.preventDefault();
+
+    if (this.state.word[this.state.word.length - 1] === this.state.value[0]) {
+      this.setState({
+        result: '딩동댕',
+        value: '',
+        word: this.state.value,
+      });
+      this.input.focus();
+    } else {
+      this.setState({
+        result: '땡',
+        value: '',
+        word: this.state.word,
+      });
+      this.input.focus();
+    }
+  };
+
+  onChangeInput = (e) => {
+    this.setState({ value: e.target.value });
+  };
+
+  input;
+
+  onRefInput = (c) => {
+    this.input = c;
+  };
+
+  // control input과 uncontrol input
+  // control input : value와 onChange 세트로 사용
+  // uncontrol input : defaultValue를 써야함
   render() {
-    return <h1>{this.state.text}</h1>
+    return (
+      <>
+        <div>{this.state.word}</div>
+        <form onSubmit={this.onSubmitForm}>
+          <input
+            ref={this.onRefInput}
+            value={this.state.value}
+            onChange={this.onChangeInput}
+            type="text"
+          />
+          <button>입력</button>
+        </form>
+        <div>{this.state.result}</div>
+      </>
+    );
   }
 }
 
 module.exports = WordRelay;
-
--- 2.끝말잇기/client.jsx
+++ 2.끝말잇기/client.jsx
@@ -1,8 +1,7 @@
 const React = require('react');
-const ReactDOM = require('react-dom');
+import { createRoot } from 'react-dom/client';
 const WordRelay = require('./WordRelay');
 
 // js와 jsx의 차이점
 // jsx는 리액트 전용 파일이라는 것을 알 수 있음
-
-ReactDOM.render(<WordRelay />, document.querySelector('#root'));
+createRoot(document.querySelector('#root')).render(<WordRelay />);
